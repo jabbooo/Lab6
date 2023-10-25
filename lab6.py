b71811e725b4ff0167296f9aa7a9e6e2e8d4244e
Hayden <haydenstone@ufl.edu>
2023-10-25 15:35:50 -0400

    final

--- lab6.py
+++ lab6.py
@@ -1,13 +1,41 @@
 
 def main():
-    user_password = input('What do you want to encode?')
-    encoder(user_password)
+    while True:
+        menu()
+        menu_choice = int(input('Please enter an option: '))
+        if menu_choice == 1:
+            user_password = input('Please enter your password to encode: ')
+            encoded_password = encoder(user_password)
+            print('Your password has been encoded and stored!')
+        elif menu_choice == 2:
+            decoder(encoded_password)
+            print(f'The encoded password is {encoded_password}, and the original password is {user_password}.')
+        elif menu_choice == 3:
+            exit()
+
+
+def menu():
+    print('''
+Menu
+-------------
+1. Encode
+2. Decode
+3. Quit
+''')
 
 def encoder(user_password):
     for num in user_password:
-        encoded_password = num + 3
-        print(encoded_password)
-
+        num = int(num)
+        num += 3
+        encoded_password += str(num)
+    return encoded_password
+    
+def decoder(encoded_password):
+    for num in encoded_password:
+        num = int(num)
+        num -= 3
+        user_password += str(num)
+    return user_password
 
 
 if __name__ == '__main__':
